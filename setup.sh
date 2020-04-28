351a8ed0b548cabc5f154e20d5a772874c7b5f97
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-04-29 01:46:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+RED='\033[0;31m'
 
 # Root Check
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo -e "${RED}This script must be run as root${NC}" 
    exit 1
 fi
 cd /root/
@@ -16,5 +17,15 @@ else
 	cd routersploit
 	python3.7 -m pip install -r requirements.txt
 	python3.7 -m pip install -r requirements-dev.txt
+fi
+
+# MCheck install
+if [[ -d /opt/MCheck ]]; then
+	sleep 1
+	echo -e "${YLW}MCheck is installed in the right place${NC}"
+else
+	echo -e "${W}Moving MCheck to the correct directory${NC}"
+	pwd && cd ..
+	mv MCheck -t /opt
 fi
 cd /opt/MCheck
