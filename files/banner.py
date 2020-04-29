c0c9562e8a51b59797bb7f54fa6c80eccfb0626a
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-04-30 00:43:11 +0200

    0.7

--- files/banner.py
+++ files/banner.py
@@ -12,7 +12,7 @@ def showbanner():
   MMM      MMM  III  KKK  KKK  RRR  RRR   OOOOOO      TTT     III  KKK  KKK
 
   MikroTik RouterOS Exploitation  ''' + "Alfa v0.7")
-    print("MCheck")
+    print("")
     print("by s1l3nt78")
     print("")
     print("Mikrotik WinBox Auth Bypass - Versions 6.29 to 6.42")
