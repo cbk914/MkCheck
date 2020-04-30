7f4e011c7b3cfc0d1b16bd08fde7a727360f9166
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-04-30 08:01:43 +0200

    0.9

--- scripts/miko.py
+++ scripts/miko.py
@@ -3,32 +3,29 @@ import paramiko
 import time
 import os
 
-
 host = "127.0.0.1"
 port = 22
-username = "admin"								# CHANGE THIS
-password = "admin"								# CHANGE THIS
+username = "admin"  # CHANGE THIS
+password = "admin"  # CHANGE THIS
 command = "/system identity print"
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(host, port, username, password)
 
-	# Invoke Shell
+# Invoke Shell
 remote_connection = ssh.invoke_shell()
-
 
 time.sleep(5)
 output = remote_connection.recv(10240)
-	
+
 print(output)
 
 # Command String (Checks the Router Network Identity)
-remote_command = ssh.exec_command(command)
-
+remote_connection = ssh.exec_command(command)
 
 time.sleep(5)
-output1 = remote_command.recv(10240)
+output1 = remote_connection.recv(10240)
 
 print(output1)
 
