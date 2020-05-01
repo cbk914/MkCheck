b95c111baef2ce139a8a12a9eeb860b63da74872
s1l3nt78 <s1l3n7h0s7@gmail.com>
2020-05-02 01:31:40 +0200

    final ver 1 ammends

--- files/banner.py
+++ files/banner.py
@@ -13,10 +13,6 @@ def showbanner():
 
   MikroTik RouterOS Exploitation  ''' + "Codename: JailBoxer")
     print("Version 1")
-    print("by s1l3nt78")
-    print("")
-    print("Mikrotik WinBox Auth Bypass - Versions 6.29 to 6.42")
-    print("Mikrotik RouterOS Jailbreak - Versions 2.9.8 up to 6.41rc56")
     exit
 
 showbanner()
--- files/banner2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def showbanner():
-    print("Mikrotik WinBox Auth Bypass - Versions 6.29 to 6.42")
-    print("Mikrotik RouterOS Jailbreak - Versions 2.9.8 up to 6.41rc56")
-    exit
-
-showbanner()
